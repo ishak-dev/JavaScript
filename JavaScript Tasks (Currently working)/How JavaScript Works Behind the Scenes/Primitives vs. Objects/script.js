6d4e5c1bd5ff1cbec1c6f794d6cf4e501730f575
ishak-dev <ishak.kazic@gmail.com>
2021-05-15 17:30:21 +0200

    Update script.js

--- JavaScript Tasks (Currently working)/How JavaScript Works Behind the Scenes/Primitives vs. Objects/script.js
+++ JavaScript Tasks (Currently working)/How JavaScript Works Behind the Scenes/Primitives vs. Objects/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage ', jessica);
+console.log('After marriage', marriedJessica);
+
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before marriage ', jessica2);
+console.log('After marriage', jessicaCopy);
