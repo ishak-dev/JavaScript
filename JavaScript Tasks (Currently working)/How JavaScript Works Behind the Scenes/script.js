150a57f66973321a010dfb1317d5c9ac35c92b2c
ishak-dev <ishak.kazic@gmail.com>
2021-05-15 14:18:52 +0200

    Update script.js

--- JavaScript Tasks (Currently working)/How JavaScript Works Behind the Scenes/script.js
+++ JavaScript Tasks (Currently working)/How JavaScript Works Behind the Scenes/script.js
@@ -2,5 +2,13 @@
 
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
+  function printAge() {
+    const output = `${firstName}, You are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
   return age;
 }
+
+const firstName = 'Jonas';
+calcAge(1991);
