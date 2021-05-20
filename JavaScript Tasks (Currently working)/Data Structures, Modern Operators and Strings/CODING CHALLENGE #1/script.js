782cd35d9420853af7361a18901dec1f223afa63
ishak-dev <ishak.kazic@gmail.com>
2021-05-20 18:17:15 +0200

    JS

--- JavaScript Tasks (Currently working)/Data Structures, Modern Operators and Strings/CODING CHALLENGE #1/script.js
+++ JavaScript Tasks (Currently working)/Data Structures, Modern Operators and Strings/CODING CHALLENGE #1/script.js
@@ -48,37 +48,35 @@ const restaurant = {
   },
 };
 
-//Use any data type, return any data type
-console.log(3 || 'Jonas');
-console.log('' || 'Jonas');
-console.log(true || 0);
-console.log(undefined || null);
+//1. Destucturing
+//Spread, because on right side of =
+const arr = [1, 2, ...[3, 4]];
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
 
-console.log(undefined || 0 || null || 'Hello' || 23 || null);
+//Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
 
-restaurant.numGuests = 23;
-const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-console.log(guests1);
+//2. Functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
 
-const guests2 = restaurant.numGuests || 10;
-console.log(guests2);
+const x = [23, 5, 7];
+add(...x);
 
-console.log('-----AND-----');
-console.log(0 && 'Jonas');
-console.log(7 && 'Jonas');
-console.log('Hello' && 23 && null && 'Jonas');
-
-if (restaurant.orderPizza) {
-  restaurant.orderPizza('mushrooms', 'spinach');
-}
-
-restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
-
-//------------------------//
-restaurant.numGuests = 0;
-const guests = restaurant.numGuests || 10;
-console.log(guests);
-//NULISH OPERATOR
-
-const guestCorrect = restaurant.numGuests ?? 10;
-console.log(guestCorrect);
+restaurant.orderPizza('mushrooms', 'onion', 'olivec', 'spinach');
+restaurant.orderPizza('mushrooms');
