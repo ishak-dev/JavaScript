c8c5564e9a8bf7bdbc15ec17245515a4374075f5
ishak-dev <ishak.kazic@gmail.com>
2021-05-20 17:51:35 +0200

    JS

--- JavaScript Tasks (Currently working)/Data Structures, Modern Operators and Strings/Short Circuiting && and II operators/script.js
+++ JavaScript Tasks (Currently working)/Data Structures, Modern Operators and Strings/Short Circuiting && and II operators/script.js
@@ -42,41 +42,15 @@ const restaurant = {
   orderPasta: function (ing1, ing2, ing3) {
     console.log(`Here is your delicious pasta with ${ing1},${ing2},${ing3}`);
   },
-  orderPizza: function (mainIngredient, ...otherIngredients) {
-    console.log(mainIngredient);
-    console.log(otherIngredients);
-  },
 };
 
-//1. Destucturing
-//Spread, because on right side of =
-const arr = [1, 2, ...[3, 4]];
-const [a, b, ...others] = [1, 2, 3, 4, 5];
-console.log(a, b, others);
-const [pizza, , risotto, ...otherFood] = [
-  ...restaurant.mainMenu,
-  ...restaurant.starterMenu,
-];
-console.log(pizza, risotto, otherFood);
+//Use any data type, return any data type
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
 
-//Objects
-const { sat, ...weekdays } = restaurant.openingHours;
-console.log(weekdays);
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
 
-//2. Functions
-const add = function (...numbers) {
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
-  }
-  console.log(sum);
-};
-add(2, 3);
-add(5, 3, 7, 2);
-add(8, 2, 5, 3, 2, 1, 4);
-
-const x = [23, 5, 7];
-add(...x);
-
-restaurant.orderPizza('mushrooms', 'onion', 'olivec', 'spinach');
-restaurant.orderPizza('mushrooms');
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
